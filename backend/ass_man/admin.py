7411763e23824463af72312b5e63d20468da892f
Rishi Tripathy <rishiraj.tripathy.33@gmail.com>
2020-01-15 18:41:23 -0500

    setup for local, need to update vars for heroku setup

---
+++ backend/ass_man/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Asset  # add this
+
+# Register your models here.
+# todo/admin.py
+
+
+class AssAdmin(admin.ModelAdmin):  # add this
+    list_display = ('title', 'description')  # add this
+
+
+# Register your models here.
+admin.site.register(Asset, AssAdmin)  # add this
