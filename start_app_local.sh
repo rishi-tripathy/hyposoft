33d04f5c18897337c38d625a91909b8d42029f7b
Rishi Tripathy <rishiraj.tripathy.33@gmail.com>
2020-01-17 00:39:52 -0500

    added startup script for local development

---
+++ start_app_local.sh
@@ -0,0 +1,22 @@
+# Run this from the hyposoft directory.
+# It installs all requirements inside a virtual env,
+# migrates changes made to the database schema,
+# starts and runs the testsuite, and launches the app locally
+
+
+# Migrating db changes
+printf "\e[42mInstalling Dependencies...\e[0m\n\n"
+pipenv install
+npm install
+
+printf "\n\n\e[42mMigrating changes...\e[0m\n\n"
+python manage.py makemigrations
+python manage.py migrate
+
+# Running tests
+printf "\n\n\e[42mRunning test suite...\e[0m\n\n"
+python manage.py test
+
+# Launching app
+printf "\n\n\e[42mRunning app locally...\e[0m\n\n"
+heroku local
