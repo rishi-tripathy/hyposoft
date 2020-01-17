b1d7b45dd212e030f9ab324a72112d3cc84272f5
Rishi Tripathy <rishiraj.tripathy.33@gmail.com>
2020-01-16 22:48:11 -0500

    updated to allow local testing

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ if os.path.isfile(dotenv_file):
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$srlc9$&yywhp7gnsdy(sg9)jk_3c23te%)n4c=%fg70t9$5y2'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
