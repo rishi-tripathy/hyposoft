7a6c014acd864603613a94d00c814940aa37d4a4
Michael Zhang <michaelzhang6284@gmail.com>
2020-01-17 12:56:36 -0500

    created a dummy landing page so we can start importing components; got rid of default CSS and put in some basic CSS for now

---
+++ src/components/Landing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Landing extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is a dummy landing page!</h1>
+      </div>
+    )
+  }
+}
+
+export default Landing
