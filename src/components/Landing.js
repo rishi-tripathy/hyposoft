260487243df8609766121b2c125df72ec84e2bb8
Michael Zhang <31636398+michaelzhang114@users.noreply.github.com>
2020-01-19 18:32:29 -0500

    Model and instance views (#19)
    * created a dummy landing page so we can start importing components; got rid of default CSS and put in some basic CSS for now
    
    * created card views for models
    
    * undeleted manage.py
    
    * added vscode to git ignore
    
    * added endline to manage.py
    
    * created dummy files for instance; same structure as model
    
    * fixed an error with namechange
    
    * created table for models
    
    * added styling to model tables
    
    * created instance table view

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import ModelController from './ModelController'
+import InstanceController from './InstanceController'
+
 
 export class Landing extends Component {
   render() {
@@ -8,6 +10,8 @@ export class Landing extends Component {
         <h1>This is a dummy landing page!</h1>
         <br></br>
         <ModelController />
+        <br></br>
+        <InstanceController />
       </div>
     )
   }
