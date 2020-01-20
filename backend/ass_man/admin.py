8bcecb19868dd58ffd5b553989f7695718fee567
Miles Todzo <mt265@duke.edu>
2020-01-19 20:44:37 -0500

    got rid of Asset references

--- backend/ass_man/admin.py
+++ backend/ass_man/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
-from .models import Asset  # add this
+from .models import Model, Instance, Rack  # add this
 
 # Register your models here.
 
 
 class AssAdmin(admin.ModelAdmin):  # add this
-    list_display = ('title', 'description')  # add this
+     list_display = ['id']  # add this
 
 
 # Register your models here.
-admin.site.register(Asset, AssAdmin)  # add this
+admin.site.register(Model, AssAdmin)  # add this
+admin.site.register(Instance, AssAdmin)
+admin.site.register(Rack, AssAdmin)
