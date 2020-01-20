eeb810bdf42333e90016c7cf827d41e87568d2dc
Christina Li <cjl56@duke.edu>
2020-01-19 19:52:27 -0500

    rack beginning arch

---
+++ src/components/RackController.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+export class RackController extends Component {
+
+    constructor() {
+    super();
+
+
+    //dummy models
+    this.state = {
+        models: [
+            {
+                id: 'A1',
+                height: 2,
+
+            }
+        ]
+    }
+
+    }
+
+  render() {
+    return (
+      <
+    )
+  }
+}
+
+export default RackController
