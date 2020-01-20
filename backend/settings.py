003f72c08c40118505618a96b0b01eb627075d49
Rishi Tripathy <rishiraj.tripathy.33@gmail.com>
2020-01-19 19:50:26 -0500

    playing with pagination

--- backend/settings.py
+++ backend/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 5
 }
 
 # Database
@@ -104,7 +104,6 @@ REST_FRAMEWORK = {
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=600)
 }
-
 
 
 # Password validation
