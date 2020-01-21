bc38fd5424ceac3c8eddc0093538b965cae1be43
Michael Zhang <michaelzhang6284@gmail.com>
2020-01-20 21:40:57 -0500

    fixed homepage in package.json

--- src/components/ModelController.js
+++ src/components/ModelController.js
@@ -1,42 +1,20 @@
 import React, { Component } from 'react'
 import ModelTable from './ModelTable'
+import axios from 'axios'
 
 export class ModelController extends Component {
   
   constructor() {
     super();
-
-    // created two dummy models
     this.state = {
-      models: [
-        {
-          id: 1,
-          vendor: 'Dell',
-          modelNumber: 'R710',
-          height: 2,
-          displayColor: 'black',
-          ethernetPorts: 4,
-          powerPorts: 1,
-          cpu: 'Intel Xeon E5520 2.2Ghz',
-          memory: 4,
-          storage: '2x500GB SSD RAID1',
-          comment: 'retired offering, no new purchasing'
-        },
-        {
-          id: 2,
-          vendor: 'Cisco',
-          modelNumber: 'R720',
-          height: 3,
-          displayColor: 'red',
-          ethernetPorts: 2,
-          powerPorts: 2,
-          cpu: 'Intel Xeon E5520 2.4Ghz',
-          memory: 4,
-          storage: '2x500GB SSD RAID2',
-          comment: 'retired offering, no new purchasing !'
-        }
-      ]
+      models: []
     }
+  }
+
+  componentDidMount() {
+    axios.get('http://0.0.0.0:5000/api/models/').then(res => {
+      this.setState({ models: res.data });
+    });
   }
   
   render() {
@@ -45,3 +23,30 @@ export class ModelController extends Component {
 }
 
 export default ModelController
+
+// {
+//   id: 1,
+//   vendor: 'Dell',
+//   modelNumber: 'R710',
+//   height: 2,
+//   displayColor: 'black',
+//   ethernetPorts: 4,
+//   powerPorts: 1,
+//   cpu: 'Intel Xeon E5520 2.2Ghz',
+//   memory: 4,
+//   storage: '2x500GB SSD RAID1',
+//   comment: 'retired offering, no new purchasing'
+// },
+// {
+//   id: 2,
+//   vendor: 'Cisco',
+//   modelNumber: 'R720',
+//   height: 3,
+//   displayColor: 'red',
+//   ethernetPorts: 2,
+//   powerPorts: 2,
+//   cpu: 'Intel Xeon E5520 2.4Ghz',
+//   memory: 4,
+//   storage: '2x500GB SSD RAID2',
+//   comment: 'retired offering, no new purchasing !'
+// }
