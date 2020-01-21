022e267216319f5cde364215bb4fe19e1362c9ef
Michael Zhang <michaelzhang6284@gmail.com>
2020-01-20 21:33:38 -0500

    can log data in testapi.js

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import ModelController from './ModelController'
 import InstanceController from './InstanceController'
+import TestAPI from './TestAPI'
 
 
 export class Landing extends Component {
@@ -9,9 +10,10 @@ export class Landing extends Component {
       <div>
         <h1>This is a dummy landing page!</h1>
         <br></br>
-        <ModelController />
+        {/* <ModelController /> */}
         <br></br>
-        <InstanceController />
+        <TestAPI />
+        {/* <InstanceController /> */}
       </div>
     )
   }
