c25a3a24094cad21219be3ea8de46d4d5ffb0594
Rishi Tripathy <40015320+rishi-tripathy@users.noreply.github.com>
2020-01-21 16:37:01 -0500

    Applying reorg to master (#24)
    * Get short (#22)
    
    * added Model model
    
    * added Instance model
    
    * added Rack model
    
    * added model serializers
    
    * added model view sets for each table
    
    * updated urls to include view endpoints
    
    * added default pagination
    
    * linted
    
    * got rid of Asset references
    
    * fixed default display color in Model model
    
    * added query param for short get of models and instances
    
    * Apply reorganized codebase to dev-backend (#21)
    
    * added hello
    
    * added local dev script
    
    * added Model model
    
    * Created view to render "Models" (#16)
    
    * created a dummy landing page so we can start importing components; got rid of default CSS and put in some basic CSS for now
    
    * created card views for models
    
    * undeleted manage.py
    
    * added vscode to git ignore
    
    * added endline to manage.py
    
    * created dummy files for instance; same structure as model
    
    * fixed an error with namechange
    
    * created table for models
    
    * added Instance model
    
    * Model and instance views (#19)
    
    * created a dummy landing page so we can start importing components; got rid of default CSS and put in some basic CSS for now
    
    * created card views for models
    
    * undeleted manage.py
    
    * added vscode to git ignore
    
    * added endline to manage.py
    
    * created dummy files for instance; same structure as model
    
    * fixed an error with namechange
    
    * created table for models
    
    * added styling to model tables
    
    * created instance table view
    
    * added Rack model
    
    * added model serializers
    
    * added model view sets for each table
    
    * updated urls to include view endpoints
    
    * added default pagination
    
    * linted
    
    * got rid of Asset references
    
    * fixed default display color in Model model
    
    * package-lock.json updated
    
    * added comma to serializer on racks
    
    * removed auth for initial API integration testing
    
    * added cors
    
    * added cors to requirements
    
    * fixed cors
    
    * can log data in testapi.js
    
    * added testAPI
    
    * fixed homepage in package.json
    
    * added cors to settings.py
    
    * integrated
    
    * fixed lil bug, integration works
    
    * initial commit with reorganization
    
    * added root package.json
    
    * rearranging complete, about to merge michael changes for integration
    
    * Integration rearrange (#23)
    
    * added cors to settings.py
    
    * Get short (#22)
    
    * added Model model
    
    * added Instance model
    
    * added Rack model
    
    * added model serializers
    
    * added model view sets for each table
    
    * updated urls to include view endpoints
    
    * added default pagination
    
    * linted
    
    * got rid of Asset references
    
    * fixed default display color in Model model
    
    * added query param for short get of models and instances
    
    * initial commit with reorganization
    
    * added root package.json
    
    * rearranging complete, about to merge michael changes for integration
    
    Co-authored-by: Miles Todzo <33913881+mtodzo@users.noreply.github.com>
    
    Co-authored-by: Michael Zhang <31636398+michaelzhang114@users.noreply.github.com>
    Co-authored-by: Christina Li <cjl56@duke.edu>
    Co-authored-by: Miles Todzo <33913881+mtodzo@users.noreply.github.com>
    
    Co-authored-by: Miles Todzo <33913881+mtodzo@users.noreply.github.com>
    Co-authored-by: Michael Zhang <31636398+michaelzhang114@users.noreply.github.com>
    Co-authored-by: Christina Li <cjl56@duke.edu>

---
+++ frontend/frontend-react/src/components/InstanceController.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import InstanceTable from './InstanceTable'
+import axios from 'axios'
+
+export class InstanceController extends Component {
+
+  constructor() {
+    super();
+    
+    // add ID
+    this.state = {
+      instances: [
+        {
+          model: 'R710',
+          hostname: 'server9',
+          rack: 'B12',
+          rack_u: 5,
+          owner: 'Michael',
+          comment: 'Reserved for Palaemon project'
+        }
+      ],
+
+    }
+  }
+
+  componentDidMount() {
+    axios.get('/api/instances/').then(res => {
+      const b = res.data.results;
+      console.log(b);
+      this.setState({ instances: b });
+    });
+  }
+
+
+  render() {
+    return <InstanceTable instances={this.state.instances} />
+  }
+}
+
+export default InstanceController
