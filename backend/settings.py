b9d993c0482b6c809147be80a5b50a6155838d35
Rishi Tripathy <rishiraj.tripathy.33@gmail.com>
2020-01-21 21:03:00 -0500

    added session auth to settings

--- backend/settings.py
+++ backend/settings.py
@@ -96,6 +96,9 @@ TEMPLATES = [
 WSGI_APPLICATION = '.wsgi.application'
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 1
 }
