7981dbfaeb788f330836daf0affa95fa57acb5d5
Miles Todzo <33913881+mtodzo@users.noreply.github.com>
2020-01-23 18:34:02 -0500

    instance creation now reflects in racks (#36)
    * instance creation now reflects in racks
    
    * fixed some typos
    
    * refactored out if tree

--- ass_man/views.py
+++ ass_man/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from rest_framework.decorators import action
 from rest_framework.response import Response
+import re
+from ass_man.models import Rack, Instance, Model
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -60,6 +63,29 @@ class InstanceViewSet(viewsets.ModelViewSet):
 
     # permission_classes = [AllowAny]
     queryset = Instance.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        model_id_str = re.search(r"models/(\d+)", request.data['model'])
+        model_id = int(model_id_str.groups()[0])
+        model = Model.objects.get(pk=model_id)
+        owner_id_str = re.search(r"users/(\d+)", request.data['owner'])
+        owner_id = int(owner_id_str.groups()[0])
+        owner = User.objects.get(pk=owner_id)
+        rack_uri = request.data['rack']
+        rack_id_str = re.search(r"racks/(\d+)", rack_uri)
+        rack_id = int(rack_id_str.groups()[0])
+        rack = Rack.objects.get(pk=rack_id)
+        instance = Instance(model=model, hostname=request.data['hostname'], rack=rack, rack_u=request.data['rack_u'], owner=owner, comment=request.data['comment'])
+        instance.save()
+
+        rack_u_str = request.data['rack_u']
+        rack_u = int(rack_u_str)
+        for i in range(model.height):
+            rack_u_field = 'u'+str(rack_u)
+            setattr(rack, rack_u_field, instance)
+            rack_u+=1
+        rack.save()
+        return super().create(request)
 
     def get_serializer_class(self):
         detail = self.request.query_params.get('detail')
