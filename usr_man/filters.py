a316862fa46d4e792319262483fba6fc2b09598b
Rishi Tripathy <rishiraj.tripathy.33@gmail.com>
2020-01-25 15:52:20 -0500

    fixed typo in user filter

--- usr_man/filters.py
+++ usr_man/filters.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class UserFilter(filters.FilterSet):
     username = filters.CharFilter(field_name='username', lookup_expr='icontains')
-    first_name = filters.CharFilter(field_name='fist_name', lookup_expr='icontains')
+    first_name = filters.CharFilter(field_name='first_name', lookup_expr='icontains')
     last_name = filters.CharFilter(field_name='last_name', lookup_expr='icontains')
     email = filters.CharFilter(field_name='email', lookup_expr='icontains')
 
