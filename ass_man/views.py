e77ece3df654a889fcfa93fc0f19ea2f2f0c1914
Miles Todzo <mt265@duke.edu>
2020-01-27 23:19:32 -0500

    made long line into two lines

--- ass_man/views.py
+++ ass_man/views.py
@@ -157,7 +157,8 @@ class InstanceViewSet(viewsets.ModelViewSet):
     def model_names(self, request, *args, **kwargs):
         name_typed = self.request.query_params.get('name') or ''
         models = Model.objects.annotate(
-        unique_name=Concat('vendor', 'model_number')).filter(unique_name__icontains=name_typed).all()
+        unique_name=Concat('vendor', 'model_number')).\
+        filter(unique_name__icontains=name_typed).all()
         serializer = UniqueModelsSerializer(models, many=True, context={'request': request})
         return Response(serializer.data)
 
