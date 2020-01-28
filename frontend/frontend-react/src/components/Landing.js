c36524d0f3fc2e17d6e6be3d2fe0d800ab9e2513
Christina Li <cjl56@duke.edu>
2020-01-28 03:24:14 -0500

    added logout functionality

--- frontend/frontend-react/src/components/Landing.js
+++ frontend/frontend-react/src/components/Landing.js
@@ -4,11 +4,21 @@ import AddUserModal from './AddUserModal'
 
 
 export class Landing extends Component {
+
+  handleLogout() {
+    window.location = "/accounts/logout/";
+    console.log(window.location);
+  }
+
+
   render() {
     return (
       <div>
         <h1>This is a dummy landing page!</h1>
         <AddUserModal />
+        <div id="LogoutButton">
+          <button onClick={this.handleLogout}>Log Out</button>
+        </div>
        </div>
     )
   }
