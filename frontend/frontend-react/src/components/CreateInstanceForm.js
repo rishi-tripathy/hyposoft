600de1d093ab893eecc6ee2d21d91eae178e7115
Michael Zhang <michaelzhang6284@gmail.com>
2020-01-30 12:35:12 -0500

    added form skeleton

--- frontend/frontend-react/src/components/CreateInstanceForm.js
+++ frontend/frontend-react/src/components/CreateInstanceForm.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
 
 export class CreateInstanceForm extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+  
+  
   render() {
     return (
-      <div>
-        <p>create instance form</p>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <h3>Create Instance Form</h3>
+        {/* <p>Model</p> <input type="text" onChange={e => this.setState({model: e.target.value})} /> */}
+        
+        <input type="submit" value="Submit" />
+      </form>
     )
   }
 }
