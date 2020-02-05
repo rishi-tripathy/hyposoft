3ebb4b9ecb2949084e862e7f0bc81f1e9a1fc7a6
Rishi Tripathy <40015320+rishi-tripathy@users.noreply.github.com>
2020-02-05 00:43:37 -0500

    remove console.log

--- frontend/frontend-react/src/components/DetailedModelModal.js
+++ frontend/frontend-react/src/components/DetailedModelModal.js
@@ -32,7 +32,6 @@ export class DetailedModelModal extends Component {
     })
     .catch(function (error) {
       // TODO: handle error
-      console.log(error.response)
       alert('Cannot load. Re-login.\n' + JSON.stringify(error.response.data, null, 2));
     });
   }
