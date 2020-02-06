6b37ff9b7fd23e8cbf5b699a301668e28ddc088e
Rishi Tripathy <40015320+rishi-tripathy@users.noreply.github.com>
2020-02-06 12:23:24 -0500

    SSL Redirect for production only (#190)
    * added secure SSL redirect
    
    * added ssl redirect for production

--- backend/settings.py
+++ backend/settings.py
@@ -41,7 +41,10 @@ def get_env_variable(var_name):
 SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# Deployment: Debug = False, SSL = True
+# Development: Debug = True, SSL = False
 DEBUG = False
+SECURE_SSL_REDIRECT = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1', '0.0.0.0', 'localhost:5000']
 
