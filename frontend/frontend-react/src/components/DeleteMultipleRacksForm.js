8cbbdb9c7c598fad1fb770059e0f43b27bbf8ba7
Christina Li <cjl56@duke.edu>
2020-02-17 21:37:14 -0500

    MUI racks (no Datacenters.. yet) (#205)
    * all current functionality in MUI. Create single and multiple racks in the same form
    
    * PR comment changes

--- frontend/frontend-react/src/components/DeleteMultipleRacksForm.js
+++ frontend/frontend-react/src/components/DeleteMultipleRacksForm.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react'
 import axios from 'axios'
-import {Button, Grid, TextField} from "@material-ui/core";
+import {
+  Grid, Button, Container, Paper, ButtonGroup, Switch, FormControlLabel, TextField
+} from "@material-ui/core"
+import {Link} from 'react-router-dom'
 
 axios.defaults.xsrfHeaderName = "X-CSRFToken";
 
@@ -28,17 +31,9 @@ export class DeleteMultipleRacksForm extends Component {
     let stateCopy = Object.assign({}, this.state);
     let stateToSend = this.removeEmpty(stateCopy);
 
-    //validate
-
-    // console.log(start_rack);
-    // console.log(end_rack);
-
     const validNumRegex = new RegExp("^[A-Z]\\d+$", 'i');
-    // console.log(validNumRegex);
 
     if (validNumRegex.test(start_rack) && validNumRegex.test(end_rack)) {
-      //  console.log(stateToSend);
-
       axios.delete('/api/racks/many/', {
         data: stateToSend
       })
@@ -46,43 +41,44 @@ export class DeleteMultipleRacksForm extends Component {
           console.log(response);
           let message = response.data.results;
           alert(response.data.results);
+          window.location = '/racks'
         })
         .catch(function (error) {
           alert('Creation was not successful.\n' + JSON.stringify(error.response.data, null, 2));
         });
     } else {
-      alert("Rack Numbers must be specified by a Single Capital Letter Followed by Multiple Numbers.");
+        alert("Rack Numbers must be specified by a Single Capital Letter Followed by Multiple Numbers.");
       return
     }
-    this.props.sendShowTable(true);
   }
 
   render() {
     let start_rack;
     let end_rack;
     return (
-      <div>
-        <Button variant="outlined" onClick={() => this.props.sendShowTable(true)}>Back</Button>{' '}
+      <Container maxwidth="s">
         <form onSubmit={this.handleSubmit}>
           <Grid container spacing={1}>
             <Grid item xs={12}>
-              <h1>Create Multiple Racks</h1>
+              <h1>Delete Racks</h1>
             </Grid>
-            <Grid item xs={6}>
+            <Grid item xs={3}>
               <TextField label='Deletion Range Start' type="text" fullWidth
                          onChange={e => this.setState({rack_num_start: e.target.value})}/>
             </Grid>
-            <Grid item xs={6}>
+            <Grid item xs={3}>
               <TextField label='Deletion Range End' type="text" fullWidth
                          onChange={e => this.setState({rack_num_end: e.target.value})}/>
             </Grid>
             <Grid item xs={12}>
-              <Button variant="contained" type="submit" color="primary" onClick={() => this.handleSubmit}>Create
-                +</Button>{' '}
+              <Button variant="contained" type="submit" color="primary" onClick={() => this.handleSubmit}>Delete
+                -</Button>{' '}
+              <Link to='/racks/'><Button variant="outlined">Cancel</Button>{' '}</Link>
+
             </Grid>
           </Grid>
         </form>
-      </div>
+      </Container>
     )
   }
 
--- frontend/frontend-react/src/components/RackRow.js
+++ frontend/frontend-react/src/components/RackRow.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import '../stylesheets/RackTable.css'
 import '../stylesheets/Printing.css'
+import {Link} from 'react-router-dom'
 
 export class RackRow extends Component {
 
@@ -24,7 +25,7 @@ export class RackRow extends Component {
   handleInstanceClick = (id) => {
 
     if (id !== null) {
-      this.props.sendFromRow(true, id);
+      // this.props.sendFromRow(true, id);
     }
   }
 
@@ -57,21 +58,25 @@ export class RackRow extends Component {
         content =
           <td style={{
             fontSize: 7,
-            background: dispColor,
+            backgroundColor: dispColor,
             color: textColor,
             borderBottom: 'solid',
           }}>
+            <Link style={{color: textColor }} to={'/assets/' + this.props.id}>
             {this.props.model} {this.displayName()}
+            </Link>
           </td>;
       } else if ((this.props.row === 42) || (!objectIsNull && this.props.hostname == null)) {
         //no display name, just display color
         content =
+        // <Link to={'/assets/' + this.props.id}>
           <td style={{
             fontSize: 7,
-            background: dispColor
+            backgroundColor: dispColor
           }}>
-
-          </td>;
+            <Link style={{color: textColor }} to={'/assets/' + this.props.id}> { } </Link>
+          </td>
+        // </Link>;
       } else if (objectIsNull) {
         //only null if 1 or 42
         if (this.props.row === '1') {
@@ -84,7 +89,6 @@ export class RackRow extends Component {
         } else if (this.props.row === '42') {
           content =
             <td>
-
             </td>;
         } else if (this.props.model === '...') {
           // ... case
@@ -110,20 +114,22 @@ export class RackRow extends Component {
         content =
           <td style={{
             fontSize: 7,
-            background: dispColor,
+            backgroundColor: dispColor,
             color: textColor,
             borderBottom: 'solid',
           }}>
+          <Link style={{color: textColor }} to={'/assets/' + this.props.id}>
             {this.props.model} {this.displayName()}
+          </Link>
           </td>;
       } else if (!objectIsNull && this.props.hostname == null) {
         content =
           <td style={{
             fontSize: 7,
-            background: dispColor,
+            backgroundColor: dispColor,
             cursor: 'pointer',
           }}>
-
+            <Link style={{color: textColor }} to={'/assets/' + this.props.id}>{ }</Link>
           </td>;
       } else {
         content = <td></td>;
@@ -140,15 +146,15 @@ export class RackRow extends Component {
     }
 
     return (
-      <tr onClick={() => this.handleInstanceClick(this.props.id)}>
-        <td>
-          {row}
-        </td>
-        {content}
-        <td>
-          {row}
-        </td>
-      </tr>
+        <tr>
+          <td>
+            {row}
+          </td>
+          {content}
+          <td>
+            {row}
+          </td>
+        </tr>
     )
   }
 }
