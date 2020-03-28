db6b2062ce9dda8491871c9ed07317e0b4ef954e
Rishi Tripathy <rishiraj.tripathy.33@gmail.com>
2020-03-28 17:47:48 -0400

    creation coded but not tested

--- cp/views.py
+++ cp/views.py
@@ -63,6 +63,110 @@ class ChangePlanViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(instance)
         return Response(serializer.data)
 
+    # Helper method for execute
+    def move_asset(self, asset, asset_cp):
+        prev_rack = asset.rack
+        prev_rack_u = asset.rack_u
+
+        new_dc = asset_cp.datacenter
+        new_rack = asset_cp.rack
+        new_rack_u = asset_cp.rack_u
+
+        asset.datacenter = new_dc
+        asset.rack = new_rack
+        asset.rack_u = new_rack
+        asset.save()
+
+        for i in range(prev_rack_u, prev_rack_u + asset.model.height + 1):
+            exec('prev_rack.u{} = None'.format(i))
+        prev_rack.save()
+
+        for i in range(new_rack_u, new_rack_u + asset.model.height):
+            exec('new_rack.u{} = asset'.format(i))
+        new_rack.save()
+
+        return
+
+    @action(detail=True, methods=['GET'])
+    def execute(self, request, *args, **kwargs):
+        # Get the change plan we're working with
+        target = self.get_object()
+        # Validate here
+        assets_cp = target.assetcp_set.first()
+        for a in assets_cp:
+            a = AssetCP() # REMOVE THIS LINE AFTER CODING
+
+            if a.id_ref: # if referencing a preexisting asset
+
+                # Handle Asset Metadata
+
+                a_ref = Asset.objects.get(id=a.id_ref)
+                # model is static
+                # hostname can change
+                a_ref.hostname = a.hostname
+                ChangePlanViewSet.move_asset(self, a_ref, a)
+
+
+                # owner can change
+                a_ref.owner = a.owner
+                # comment can change
+                a_ref.comment = a.comment
+                # asset number can change (how do we want to handle this??)
+
+                a_ref.save()
+            else:
+                a_new = Asset(model=a.model, hostname=a.hostname, datacenter=a.datacenter,
+                              rack=a.rack, rack_u = a.rack_u, owner=a.owner, comment=a.comment,
+                              asset_number=a.asset_number)
+                ChangePlanViewSet.move_asset(self, a_new, a)
+                a_new.save()
+                a.id_ref=a_new.pk #when doing network ports, now have a way to access to the real object
+        # Make all assets before doing NP or CP
+
+
+        # Handle Network Ports for this CP Asset
+        for n in target.npcp_set.filter(asset_cp_id=a):
+            if n.id_ref: # if referencing existing np
+                n_ref = Network_Port.objects.get(id=n.id_ref)
+
+                # name may change
+                n_ref.name = n.name
+                # mac may change
+                n_ref.mac = n.mac
+                # connection may change
+                n_ref.connection = n.connection # This assumes that the CPNP n is planned to be connected to a preexisting NP
+                n.connection = n_ref
+
+                n_ref.save()
+                n.connection.save()
+
+                # not sure how to process connected to another new NP (created in CP)
+
+                # asset cannot change
+
+            else: # creating new NPs
+
+                n_new = Network_Port(name=n.name, mac=n.mac, connection=n.connection,
+                                     asset=Asset.objects.get(AssetCP.objects.get(id=n.asset_cp_id).id_ref)) # This assumes that the CPNP n is planned to be connected to a preexisting NP
+
+                n_new.save()
+
+
+        # Handle Power Ports for this CP Asset
+        for p in target.ppcp_set.filter(assets_cp_id=a):
+            if p.id_ref: # if referencing existing pp
+
+            else: #creating new PP
+                p_new = Power_Port(name="", pdu=p.pdu, port_number=p.port_number,
+                                   asset=Asset.objects.get(AssetCP.objects.get(id=n.asset_cp_id).id_ref))
+
+                p_new.save()
+
+
+        return Response({
+            'hostname': assets_cp.hostname
+        })
+
 
 class AssetCPViewSet(viewsets.ModelViewSet):
 
