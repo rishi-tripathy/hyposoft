d0d5308e507c3992ce212b349ec14ccbe79f7a25
Miles Todzo <mt265@duke.edu>
2020-03-28 10:56:32 -0700

    added auth check for api/decommissioned/

--- ass_man/views/DecommissionedViewSet.py
+++ ass_man/views/DecommissionedViewSet.py
@@ -4,12 +4,14 @@ from ass_man.serializers.decommissioned_serializers import DecommissionedSeriali
 from ass_man.filters import DecommissionedFilter
 from django_filters import rest_framework as djfiltBackend
 from rest_framework.filters import OrderingFilter
+from rest_framework.permissions import IsAuthenticated
 
 DECOMMISSIONED_ORDERING_FILTERING_FIELDS = ['username', 'timestamp']
 
 class DecommissionedViewSet(viewsets.ModelViewSet):
     queryset = Decommissioned.objects.all()
     serializer_class = DecommissionedSerializer
+    permission_classes = [IsAuthenticated]
     http_method_names = ['get']
     ordering_fields = DECOMMISSIONED_ORDERING_FILTERING_FIELDS
     ordering = ['username', 'timestamp']
